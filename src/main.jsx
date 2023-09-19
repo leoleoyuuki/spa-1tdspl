0764a2a5a639f2f38c2f12c4b9912c6e6974a903
leoleoyuuki <rm550373@fiap.com.br>
2023-09-19 09:53:38 -0300

    useEffects

--- src/main.jsx
+++ src/main.jsx
@@ -28,7 +28,7 @@ const router = createBrowserRouter([
 // BLOCO DE ROTAS 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
+  // </React.StrictMode>,
 )
--- src/routes/Produtos.jsx
+++ src/routes/Produtos.jsx
@@ -4,14 +4,43 @@ import { ListaProdutos } from "../components/ListaProdutos"
 import classes from './Produtos.module.css'
 import {AiFillEdit as Editar} from 'react-icons/ai'
 import {RiDeleteBin2Fill as Excluir} from 'react-icons/ri'
+import { useEffect, useState } from "react"
 
 
 export default function Produtos(){
+
+    
+
+  const[counter,setCounter] = useState(0);
+    document.title = "Lista de produtos: " + counter
+
+
+    const [listaProdutosLocal, setlistaProdutosLocal] = useState([{ListaProdutos}]);
+
+    useEffect(()=>{
+        console.log("Este useEffect renderiza sempre que ocorre alguma atualização no componente!")
+    })
+
+    useEffect(()=>{
+        console.log("Este useEffect renderiza apenas uma vez atualização no componente!")
+    },[]);
+
+    const[counter2,setCounter2] = useState(0);
+    useEffect(()=>{
+        console.log("Este useEffect renderiza apenas se o objeto/elemento/componente e ou variavel sofrer uma atualização. devemos colocar o item a ser monitrado no array de dependencias: [ ]")
+    },[counter2]);
+
+
     return(
         <>
         <h1>Produtos de INFORMATICA - FIPAPI</h1>
         <p>Os melhores produtos mercado</p>
-
+        <div>
+            <button onClick={()=>setCounter(counter + 1)}>Counter - {counter}</button>
+        </div>
+        <div>
+            <button onClick={()=>setCounter2(counter + 1)}>Counter - {counter2}</button>
+        </div>
         <table className={classes.estilo}>
             <thead className={classes.tableHeaders}>
                 <tr>
@@ -26,7 +55,7 @@ export default function Produtos(){
                 </tr>
             </thead>
             <tbody>
-                {ListaProdutos.map((produto, indice)=>(
+                {listaProdutosLocal.map((produto, indice)=>(
                     <tr key={indice} className={classes.tableLine}>
                         <td>{produto.id}</td>
                         <td>{produto.nome}</td>
