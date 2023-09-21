6994748a9d0065a4592e9afabc9b9ba4e28d4e27
leoleoyuuki <rm550373@fiap.com.br>
2023-09-21 09:29:32 -0300

    Id adicionado corretamente

--- src/routes/InserirProdutos.jsx
+++ src/routes/InserirProdutos.jsx
@@ -1,42 +1,67 @@
 import { useState } from "react";
 import {ListaProdutos} from "../components/ListaProdutos";
+import { useNavigate } from "react-router-dom";
 
 export default function InserirProduto() {
     document.title = 'Inserir Produto';
 
+    const navigate = useNavigate();
+
     const [produtoId, setProdutoId] = useState(0);
+    let idProd = parseInt(ListaProdutos[ListaProdutos.length-1].id) + 1;
     const [produto, setProduto] = useState({
-        id: 0,
+        id: idProd,
         nome: "",
         desc: "",
         preco: 0.0,
         img: ""
     });
     
+    const handleChange = (e) => {
+        e.preventDefault();
+        const {name, value} = e.target;
+        setProduto({...produto, [name]: value});
+    }
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        
+        // setProdutoId(parseInt(ListaProdutos[ListaProdutos.length - 1].id + 1));
+        // setProduto({...produto,id:produtoId})
+        
+        ListaProdutos.push(produto);
 
+        alert("Produto inserido com sucesso!");
+        navigate("/produtos");
+    };
+
+    const handleId = (e) => {
+        e.preventDefault();
+        setProdutoId(parseInt(ListaProdutos[ListaProdutos.length - 1].id + 1));
+        setProduto({...produto,id:produtoId})
+    }
   return (
     <div>
         <h1>Inserir Produto</h1>
         <div>
-            <form >
+            <form onSubmit={handleSubmit}>
                 <fieldset>
                     <legend>Cadastrar Produto</legend>
                     <div>
                         <label htmlFor="idNome">Nome: </label>
-                        <input type="text" name="nome" id="idNome" value={produto.nome}/>
+                        <input type="text" name="nome" id="idNome" value={produto.nome} onChange={handleChange}/>
                     </div>
                     <div>
                         <label htmlFor="idDesc">Descrição : </label>
-                        <input type="text" name="desc" id="idDesc" value={produto.desc}/>
+                        <input type="text" name="desc" id="idDesc" value={produto.desc} onChange={handleChange}/>
                     </div>
                     <div>
                         <label htmlFor="idPreco">Preço : </label>
-                        <input type="number" name="preco" id="idPreco" value={produto.preco}/>
+                        <input type="number" name="preco" id="idPreco" value={produto.preco} onChange={handleChange}/>
                     </div>
                     <div>
                         <label htmlFor="idImg">Imagem : </label>
-                        <input type="url" name="img" id="idImg" value={produto.img}/>
+                        <input type="url" name="img" id="idImg" value={produto.img} onChange={handleChange}/>
                     </div>
                     <div>
                         <button>Cadastrar</button>
